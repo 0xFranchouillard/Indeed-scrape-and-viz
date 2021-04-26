ee94357aa7e904578504f701c70a322252337576
CyrilleChampion <cyrillechampion13@gmail.com>
2021-04-26 14:36:59 +0200

    Merge separated Flask & Scraping part optimize scraping part (findAll -> find)
    only one job per page for now (problems with captcha & time with testing)

--- scraping_functions.py
+++ scraping_functions.py
@@ -1,11 +1,7 @@
 import requests
-import bs4
 from bs4 import BeautifulSoup
-import pandas as pd
 import time
 import json
-import pprint
-from pymongo import MongoClient
 from config import headers,URLJob
 
 def make_soup(URL):
@@ -22,20 +18,15 @@ def make_soup(URL):
     return soup
 
 def extract_data_from_jobs_pages(job, div):
-    for jobName in div.find_all(name='a', attrs={'data-tn-element': 'jobTitle'}):
-        job['jobName'] = jobName['title']
+    job['jobName'] = div.find(name='a', attrs={'data-tn-element': 'jobTitle'})['title']
 
-    for companyName in div.find_all(name='span', attrs={'class': 'company'}):
-        job['companyName'] = companyName.text.strip()
+    job['companyName'] = div.find(name='span', attrs={'class': 'company'}).text.strip()
 
-    for cityName in div.findAll(name='span', attrs={'class': 'location accessible-contrast-color-location'}):
-        job['cityName'] = cityName.text.strip()
+    job['cityName'] = div.find(name='span', attrs={'class': 'location accessible-contrast-color-location'}).text.strip()
 
-    for jobShortDescription in div.findAll('div', attrs={'class': 'summary'}):
-        job['jobShortDescription'] = jobShortDescription.text.strip()
+    job['jobShortDescription'] = div.find('div', attrs={'class': 'summary'}).text.strip()
 
-    for jobRating in div.findAll(name='span', attrs={'class': 'ratingsContent'}):
-        job['rating'] = jobRating.text.strip()
+    job['rating'] = div.find(name='span', attrs={'class': 'ratingsContent'}).text.strip()
 
     job['jobLink'] = div['data-jk']
 
@@ -48,24 +39,22 @@ def extract_data_from_jobs_pages(job, div):
     return job
 
 def extract_data_from_job_page(job, div):
-    for jobType in div.findAll(name='span', attrs={'class': 'jobsearch-JobMetadataHeader-item icl-u-xs-mt--xs'}):
-        jobTypes = jobType.text.strip().strip('-  ').split(',')
-        jobTypesSplit = []
-        for type in jobTypes:
-            jobTypesSplit.append(type.lstrip())
-    for jobSalary in div.findAll(name='span', attrs={'class': 'icl-u-xs-mr--xs'}):
-        job['jobSalary'] = jobSalary.text.strip()
+    job['jobType'] = div.find(name='span', attrs={'class': 'jobsearch-JobMetadataHeader-item icl-u-xs-mt--xs'}).text.strip()
+    job['jobSalary'] = div.find(name='span', attrs={'class': 'icl-u-xs-mr--xs'}).text.strip()
     return job
 
 def get_job_data_from_job_soup(soup):
-    for div in soup.find_all(name='div', attrs={'class': 'jobsearch-JobMetadataHeader-item'}):
-        job_data = dict()
-        job = extract_data_from_job_page(job_data, div)
+    job_data = dict()
+    job = extract_data_from_job_page(job_data, soup.find(name='div', attrs={'class': 'jobsearch-JobMetadataHeader-item'}))
     return json.dumps(job)
 
 def get_jobs_pages_from_page_soup(jobs, soup):
-    for div in soup.find_all(name='div', attrs={'class': 'row'}):
+    """for div in soup.find_all(name='div', attrs={'class': 'row'}):
         job = dict()
         job = extract_data_from_jobs_pages(job, div)
-        jobs.append(job)
+        jobs.append(job)"""
+    div = soup.find(name='div', attrs={'class': 'row'})
+    job = dict()
+    job = extract_data_from_jobs_pages(job, div)
+    jobs.append(job)
     return jobs
